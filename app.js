a5fd84dd9aa2a4bea8806ad0e5109e0a93ec1149
sylee0201 <gksdiddueo@naver.com>
2016-09-08 13:29:28 +0900

    ejs test

--- app.js
+++ app.js
@@ -10,7 +10,29 @@ var app=express();
 app.use(express.static(__dirname+'/public'));
 */
 
-app.use(express.static(path.join(__dirname,'public')));
+app.set("view engine",'ejs');
+//app.use(express.static(path.join(__dirname,'public')));
+
+var data={count:0};
+app.get('/',function(req,res){
+  data.count++;
+  res.render('my_first_ejs',data);
+});
+
+app.get('/reset',function(req,res){
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+
+app.get('/set/count',function(req,res){
+  if(req.query.count)data.count=req.query.count;
+  res.render('my_first_ejs',data);
+});
+
+app.get('/set/:num',function(req,res){
+  data.count=req.params.num;
+  res.render('my_first_ejs',data);
+});
 
 app.listen(3000,function(){
   console.log('server on!!');
