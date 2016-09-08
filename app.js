07274af3c348efb494ec113b150eebc98a0c1cd1
sylee0201 <gksdiddueo@naver.com>
2016-09-08 12:58:04 +0900

    express index html

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
-
+var path = require('path');
 var app=express();
 
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
   res.send('hello world');
 });
+
+
+app.use(express.static(__dirname+'/public'));
+*/
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(3000,function(){
   console.log('server on!!');
